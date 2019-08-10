520c80ae9d04bea5a0b3d6c1f55783ac0dbf51a8
hongzhi guo <iaccepted@163.com>
2019-08-10 22:30:38 +0800

    add structure of vhost_user_msg
    Signed-off-by: hongzhi guo <iaccepted@163.com>

---
+++ vhost_user/vhost_user.c
@@ -0,0 +1,202 @@
+#include <sys/uio.h>
+#include <sys/socket.h>
+
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#include "vhost_user.h"
+
+/*return bytes# of read on success or negative val on failure.
+  read vhost_user_msg header and fds in ancillary data
+*/
+static int read_fd_and_message_hdr(int sockfd, char *buf, int buflen, int *fds, int fd_num)
+{
+    struct iovec iov;
+    struct msghdr msgh;
+    struct cmsghdr *cmsgh = NULL;
+    int control_size = fd_num * sizeof(int);
+    char control_data[CMSG_SPACE(control_size)];
+    int ret;
+
+    memset(control_data, 0, sizeof(control_data));
+    memset(&msgh, 0, sizeof(struct msghdr));
+
+    iov.iov_base = buf;
+    iov.iov_len = buflen;
+    msgh.msg_iov = &iov;
+    msgh.msg_iovlen = 1;
+    msgh.msg_control = (void *)control_data;
+    msgh.msg_controllen = sizeof(control_data);
+
+    ret = recvmsg(sockfd, &msgh, 0);
+    if (ret <= 0) {
+        /* error log */
+        return ret;
+    }
+
+     if (msgh.msg_flags & (MSG_TRUNC | MSG_CTRUNC)) {
+        /* error log */
+        return -1;
+    }
+
+    for (cmsgh = CMSG_FIRSTHDR(&msgh); cmsgh != NULL;
+         cmsgh = CMSG_NXTHDR(&msgh, cmsgh)) {
+         if (cmsgh->cmsg_level == SOL_SOCKET &&
+             cmsgh->cmsg_type == SCM_RIGHTS) {
+             memcpy(fds, CMSG_DATA(cmsgh), cmsgh->cmsg_len - CMSG_LEN(0));
+             break;
+         }
+    }
+
+    return ret;
+}
+
+/* read message from qemu */
+static int read_vhost_message(int sockfd, vhost_user_msg *msg)
+{
+    int ret;
+
+    ret = read_fd_and_message_hdr(sockfd, (char *)msg, VHOST_USER_HDR_SIZE,
+        msg->fds, VHOST_MEMORY_MAX_NREGIONS);
+    if (ret <= 0)
+        return ret;
+
+    if (msg && msg->size) {
+        if (msg->size > sizeof(msg->payload)) {
+            /* error log */
+            return -1;
+        }
+
+        /* read the message body */
+        ret = read(sockfd, &msg->payload, msg->size);
+        if (ret <= 0)
+            return ret;
+        if (ret != (int)msg->size) {
+            /* error log */
+            return -1;
+        }
+    }
+
+    return ret;
+}
+
+static int send_fd_and_message(int fd, char *buf, int buf_len, int *fds, int fd_num)
+{
+    struct iovec iov;
+    struct msghdr msgh;
+    int control_size = fd_num * sizeof(int);
+    char control_data[CMSG_SPACE(control_size)];
+    struct cmsghdr *cmsg = NULL;
+    int ret;
+
+    memset(&msgh, 0, sizeof(msgh));
+    memset(control_data, 0, sizeof(control_data));
+    iov.iov_base = buf;
+    iov.iov_len = buf_len;
+    msgh.msg_iov = &iov;
+    msgh.msg_iovlen = 1;
+
+    if (fds && fd_num > 0) {
+        msgh.msg_control = control_data;
+        msgh.msg_controllen = sizeof(control_data);
+        cmsg = CMSG_FIRSTHDR(&msgh);
+        if (cmsg == NULL) {
+            errno = EINVAL;
+            return -1;
+        }
+        cmsg->cmsg_len = CMSG_LEN(control_size);
+        cmsg->cmsg_level = SOL_SOCKET;
+        cmsg->cmsg_type = SCM_RIGHTS;
+        memcpy(CMSG_DATA(cmsg), fds, control_size);
+    }
+
+    do {
+        ret = sendmsg(fd, &msgh, 0);
+    } while (ret < 0 && errno == EINTR);
+
+    if (ret < 0) {
+        return ret;
+    }
+
+    return ret;
+}
+
+static int send_vhost_message(int sockfd, struct vhost_user_msg *msg)
+{
+    if (!msg)
+        return 0;
+
+    return send_fd_and_message(sockfd, (char *)msg,
+        VHOST_USER_HDR_SIZE + msg->size, NULL, 0);
+}
+
+static int send_vhost_reply(int sockfd, struct vhost_user_msg *msg)
+{
+    if (!msg)
+        return 0;
+
+    /* clear the low 3 bit, then set (VHOST_USER_VERSION | VHOST_USER_REPLY_MASK) */
+    msg->flags &= ~VHOST_USER_VERSION_MASK;
+    msg->flags &= ~VHOST_USER_NEED_REPLY;
+    msg->flags |= (VHOST_USER_VERSION | VHOST_USER_REPLY_MASK);
+
+    return send_vhost_message(sockfd, msg);
+}
+
+int vhost_user_msg_handler(int fd)
+{
+    struct vhost_user_msg msg;
+    int ret;
+
+    ret = read_vhost_message(fd, &msg);
+    if (ret <= 0) {
+        if (ret < 0) {
+        } else {
+        }
+        return -1;
+    }
+
+    if (msg.request > VHOST_USER_MAX) {
+        return -1;
+    }
+
+    switch(msg.request) {
+        case VHOST_USER_GET_FEATURES:
+            break;
+        case VHOST_USER_SET_FEATURES:
+            break;
+        case VHOST_USER_SET_OWNER:
+            break;
+        case VHOST_USER_RESET_OWNER:
+            break;
+        case VHOST_USER_SET_MEM_TABLE:
+            break;
+        case VHOST_USER_SET_LOG_BASE:
+            break;
+        case VHOST_USER_SET_LOG_FD:
+            break;
+        case VHOST_USER_SET_VRING_NUM:
+            break;
+        case VHOST_USER_SET_VRING_ADDR:
+            break;
+        case VHOST_USER_SET_VRING_BASE:
+            break;
+        case VHOST_USER_SET_VRING_KICK:
+            break;
+        case VHOST_USER_SET_VRING_CALL:
+            break;
+        case VHOST_USER_SET_VRING_ERR:
+            break;
+        case VHOST_USER_GET_PROTOCOL_FEATURES:
+            break;
+        case VHOST_USER_SET_PROTOCOL_FEATURES:
+            break;
+        case VHOST_USER_GET_QUEUE_NUM:
+            break;
+        case VHOST_USER_SET_VRING_ENABLE:
+            break;
+        default:
+            break;
+    }
+}
---
+++ vhost_user/vhost_user.h
@@ -0,0 +1,96 @@
+#include <inttypes.h>
+
+/* refer to qemu 4.0 hw/virtio/vhost_user.h */
+#define VHOST_MEMORY_MAX_NREGIONS 8
+#define VHOST_USER_VERSION 0x1
+
+typedef enum vhost_user_request {
+    VHOST_USER_NONE = 0,
+    VHOST_USER_GET_FEATURES = 1,
+    VHOST_USER_SET_FEATURES = 2,
+    VHOST_USER_SET_OWNER = 3,
+    VHOST_USER_RESET_OWNER = 4,
+    VHOST_USER_SET_MEM_TABLE = 5,
+    VHOST_USER_SET_LOG_BASE = 6,
+    VHOST_USER_SET_LOG_FD = 7,
+    VHOST_USER_SET_VRING_NUM = 8,
+    VHOST_USER_SET_VRING_ADDR = 9,
+    VHOST_USER_SET_VRING_BASE = 10,
+    VHOST_USER_GET_VRING_BASE = 11,
+    VHOST_USER_SET_VRING_KICK = 12,
+    VHOST_USER_SET_VRING_CALL = 13,
+    VHOST_USER_SET_VRING_ERR = 14,
+    VHOST_USER_GET_PROTOCOL_FEATURES = 15,
+    VHOST_USER_SET_PROTOCOL_FEATURES = 16,
+    VHOST_USER_GET_QUEUE_NUM = 17,
+    VHOST_USER_SET_VRING_ENABLE = 18,
+    VHOST_USER_MAX,
+} vhost_user_request;
+
+typedef struct vhost_user_memory_region {
+    uint64_t guest_phys_addr;
+    uint64_t memory_size;
+    uint64_t userspace_addr;
+    uint64_t mmap_offset;
+} vhost_user_memory_region;
+
+typedef struct vhost_user_memory {
+    uint32_t nregions;
+    uint32_t padding;
+    vhost_user_memory_region regions[VHOST_MEMORY_MAX_NREGIONS];
+} vhost_user_memory;
+
+typedef struct vhost_vring_state {
+    unsigned int index;
+    unsigned int num;
+} vhost_vring_state;
+
+typedef struct vhost_vring_addr {
+    unsigned int index;
+    /* Option flags:now used to store vhost_user_version */
+    unsigned int flags;
+
+    /* Whether log address is valid. If set enables logging. */
+#define VHOST_VRING_F_LOG 0
+
+    /* desc ring address */
+    uint64_t desc_user_addr;
+    /* used ring addr*/
+    uint64_t used_user_addr;
+    /* avail ring address */
+    uint64_t avail_user_addr;
+    /* log  */
+    uint64_t log_guest_addr;
+} vhost_vring_addr;
+
+typedef struct vhost_user_log {
+    uint64_t mmap_size;
+    uint64_t mmap_offset;
+} vhost_user_log;
+
+typedef struct vhost_user_msg {
+    vhost_user_request request;
+
+#define VHOST_USER_VERSION_MASK     0x3
+#define VHOST_USER_REPLY_MASK       (0x1 << 2)
+#define VHOST_USER_NEED_REPLY       (0x1 << 3)
+    uint32_t flags;
+    uint32_t size; /* the following payload size */
+    union {
+#define VHOST_USER_VRING_IDX_MASK   0xff
+#define VHOST_USER_VRING_NOFD_MASK  (0x1<<8)
+        uint64_t u64;
+        vhost_vring_state state;
+        vhost_vring_addr addr;
+        vhost_user_memory memory;
+        vhost_user_log log;
+
+        /*TODO:support iotlb for no iommu platform */
+        //struct vhost_iotlb_msg iotlb;
+    } payload;
+    int fds[VHOST_MEMORY_MAX_NREGIONS];
+} __attribute((packed)) vhost_user_msg;
+
+#define VHOST_USER_HDR_SIZE ((size_t) &(((vhost_user_msg *)0)->payload.u64))
+
+int vhost_user_msg_handler(int fd);
